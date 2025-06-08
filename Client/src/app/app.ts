5dc7ff19be2f203e0b3de00109ba7ea8a14ad767
Atlas2234 <dlz988675@gmail.com>
2025-06-07 23:57:51 -0400

    set up basic HTTP request to employee mock API

--- Client/src/app/app.ts
+++ Client/src/app/app.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -7,6 +8,21 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit{
+  http = inject(HttpClient);
   protected title = 'Client';
+
+  ngOnInit(): void {
+    this.http.get('https://my.api.mockaroo.com/employees?key=b06ce4d0').subscribe({
+      next: (data) => {
+        console.log('Response from server:', data);
+      }
+      , error: (error) => {
+        console.error('Error fetching data from server:', error);
+      }
+      , complete: () => {
+        console.log('Request completed');
+      }
+    })
+  }
 }
