e1db921cbb0d8bdf48bf756ebd31f7b1e276b07c
Atlas2234 <dlz988675@gmail.com>
2025-06-09 21:35:00 -0400

    added custom favicon and mockup images for the login screen

--- Client/src/app/app.ts
+++ Client/src/app/app.ts
@@ -1,20 +1,24 @@
+import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Nav } from "./nav/nav";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, Nav],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App implements OnInit{
   http = inject(HttpClient);
   protected title = 'Client';
+  public users: any;
 
   ngOnInit(): void {
     this.http.get('https://my.api.mockaroo.com/employees?key=b06ce4d0').subscribe({
       next: (data) => {
+        this.users = signal(data);
         console.log('Response from server:', data);
       }
       , error: (error) => {
